bc7d21607e6e0dccec0ef1b893f34173b749d7e5
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-09-27 11:17:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,23 @@
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
 }
 
 function draw() {
+  background(200,200,200)
+  
+  randomSeed(99)
+  for (let i = 0; i < 25; i++) {
+    let x1= random(100)
+    let x2= random(600)
+    let y1= random (600)
+    let y2= random (600)
+
+stroke(0,0,0)    
+line(x1,y1,x2,y2)
+  }
 }
+
+
+  
